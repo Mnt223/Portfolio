28e95562a22092286eed76e84998448237402ce1
Mnt223 <134673686+Mnt223@users.noreply.github.com>
2025-06-05 21:46:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function showSection(id) {
-  document.querySelectorAll('.popup').forEach(p => p.style.display = 'none');
-  document.getElementById(id).style.display = 'block';
-}
 
-function closePopup(id) {
-  document.getElementById(id).style.display = 'none';
-}
-
-particlesJS.load('particles-js', 'particles.json', function () {
-  console.log('Particles loaded.');
-});
